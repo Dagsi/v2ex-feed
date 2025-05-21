2fc2c312520513edbaad24fca7bb6bdb7afd0f9d
linguocheng <1390840865@qq.com>
2025-05-21 16:10:52 +0800

    优化

--- src/v2ex_feed/telegram_utils.py
+++ src/v2ex_feed/telegram_utils.py
@@ -67,7 +67,7 @@ class PostPayload:
         if self.node_name:
             raw = "".join(self.node_name.split()).replace("#", "")
             tag = html.escape(raw).strip()
-            node_line = f"标签: #{tag}{settings.TELEGRAM_CHAT_USERNAME}"
+            node_line = f"标签: #{tag}"
         else:
             node_line = None
 
